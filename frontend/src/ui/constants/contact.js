65acb1c9a9397a916cc86380946529d3cf235b55
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-01-03 23:25:41 +0100

    [US #53] Location map and full address.

--- frontend/src/ui/constants/contact.js
+++ frontend/src/ui/constants/contact.js
@@ -1,4 +1,6 @@
 export const ADDRESS =
-  "Time Banking (My Company) " +
-  "\n (whole address) " +
-  "\n POLAND";
+  "Time Banking" +
+  "\n AGH University of Science and Technology" +
+  "\n al. Mickiewicza 30" +
+  "\n 30-059 Krakow" +
+  "\n Poland";
--- frontend/src/ui/container/ContactPage.js
+++ frontend/src/ui/container/ContactPage.js
@@ -6,6 +6,29 @@ import {ADDRESS} from "../constants/contact";
 
 export class ContactPage extends Component {
 
+  componentDidMount() {
+    const latitude = 50.0645191000000000;
+    const longitude = 19.923639699999967;
+    const description = 'aleja Adama Mickiewicza 30, Kraków, Polska';
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {
+        lat: latitude,
+        lng: longitude
+      },
+      zoom: 12,
+      mapTypeId: 'roadmap',
+    });
+    new window.google.maps.Marker({
+      map: map,
+      position: {
+        lat: latitude,
+        lng: longitude
+      },
+      title: description,
+      zIndex: 1
+    });
+  }
+
   render() {
     return (
       <div className="main">
@@ -21,11 +44,16 @@ export class ContactPage extends Component {
             </div>
             <div className="paragraph">
               <h3>Address</h3>
-              <div>
-                <p>You can also write to:</p>
-                <p className="bold">
-                  {renderText(ADDRESS)}
-                </p>
+              <div className="address-view">
+                <div className="column">
+                  <p>You can also write to:</p>
+                  <p className="bold">
+                    {renderText(ADDRESS)}
+                  </p>
+                </div>
+                <div className="column">
+                  <div id="map"/>
+                </div>
               </div>
             </div>
           </div>
